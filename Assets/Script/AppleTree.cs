07f454a702ecee4370bd27bf59b71d67a46613cc
unrich <60159029+unrich@users.noreply.github.com>
2020-09-18 19:16:40 -0400

    Step 3
    create AppleTree Script

---
+++ Assets/Script/AppleTree.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    //Prefab for instantiating apples
+    public GameObject applePrefab;
+
+    //Speed at which the AppleTree moves
+    public float speed = 1f;
+
+    //Distance where AppleTree turns around
+    public float leftAndRightEdge = 10f;
+
+    //Chance that the AppleTree will change directions
+    public float chanceToChangeDirections = 0.1f;
+
+    //Rate at which Apples will be instantiated
+    public float secondBetweenAppleDrops = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Droppping apples every second    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Basic Movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Timeout.deltaTime;
+        transform.position = pos;
+
+        //Changing Direction
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed); //move right
+        }else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed); //move left
+        }else if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1; //change direction
+        }
+    }
+}
